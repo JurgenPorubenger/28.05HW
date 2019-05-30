6f3bc52c3b617d5e5422e131e0b86cccb2a41c2c
Eugene <jurgen.porubenger@gmail.com>
2019-05-30 17:27:09 +0300

    last

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ alarmButton.addEventListener("click", function () {
     greenLight.classList.add("red");
     greenLight.classList.remove("green");
 
+
     let redLightSecond = document.querySelector(".red_second");
     let greenLightSecond = document.querySelector(".green_second");
 
@@ -17,12 +18,13 @@ alarmButton.addEventListener("click", function () {
     greenLightSecond.classList.add("red_second");
     greenLightSecond.classList.remove("green_second");
 
-    let horizontal = document.querySelector(".horizontal_road");
-    let vertical = document.querySelector(".vertical_road");
+    let greenRoad = document.querySelector(".green_road");
+    let redRoad = document.querySelector(".red_road");
 
-    horizontal.classList.add("red_road");
-    horizontal.classList.remove(".horizontal_road");
-    vertical.classList.add("green_road");
-    vertical.classList.remove("vertical_road");
-    console.log(horizontal)
+    greenRoad.classList.add("red_road");
+    console.log(greenRoad);
+    greenRoad.classList.remove("green_road");
+    // console.log(horizontal);
+    redRoad.classList.add("green_road");
+    redRoad.classList.remove("red_road");
 })
