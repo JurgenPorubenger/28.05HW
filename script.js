2f2d4a2b9bc7beb970c645f69b2e2813928b2dd3
Eugene <jurgen.porubenger@gmail.com>
2019-06-05 12:39:03 +0300

    proto

--- script.js
+++ script.js
@@ -1,85 +1,128 @@
-// let alarmButton = document.querySelector(".alarm_button");
-// alarmButton.addEventListener("click", function () {
+// // let alarmButton = document.querySelector(".alarm_button");
+// // alarmButton.addEventListener("click", function () {
 
-//     let redLight = document.querySelector(".red");
-//     let greenLight = document.querySelector(".green");
+// //     let redLight = document.querySelector(".red");
+// //     let greenLight = document.querySelector(".green");
 
-//     redLight.classList.add("green");
-//     redLight.classList.remove("red");
-//     greenLight.classList.add("red");
-//     greenLight.classList.remove("green");
-
-
-//     let redLightSecond = document.querySelector(".red_second");
-//     let greenLightSecond = document.querySelector(".green_second");
-
-//     redLightSecond.classList.add("green_second");
-//     redLightSecond.classList.remove("red_second");
-//     greenLightSecond.classList.add("red_second");
-//     greenLightSecond.classList.remove("green_second");
-
-//     let greenRoad = document.querySelector(".green_road");
-//     let redRoad = document.querySelector(".red_road");
-
-//     greenRoad.classList.add("red_road");
-//     greenRoad.classList.remove("green_road");
-//     redRoad.classList.add("green_road");
-//     redRoad.classList.remove("red_road");
-// })
+// //     redLight.classList.add("green");
+// //     redLight.classList.remove("red");
+// //     greenLight.classList.add("red");
+// //     greenLight.classList.remove("green");
 
 
-// let alarmButton = document.querySelector(".alarm_button");
-// alarmButton.addEventListener("click", function () {
-//     let redLight = document.querySelector("#redId");
-//     let greenLight = document.querySelector("#greenId");
-//     let redLight2 = document.querySelector("#redId2");
-//     let greenLight2 = document.querySelector("#greenId2");
-//     let greenRoad = document.querySelector(".green_road");
-//     let redRoad = document.querySelector(".red_road");
-//     if (redLight.classList.contains("red")||redLight.classList.contains("green")&&greenLight.classList.contains("green")||greenLight.classList.contains("red")) {
-//         redLight.classList.toggle("green");
-//         redLight.classList.toggle("red");
-//         greenLight.classList.toggle("red");
-//         greenLight.classList.toggle("green");
+// //     let redLightSecond = document.querySelector(".red_second");
+// //     let greenLightSecond = document.querySelector(".green_second");
 
-//         redLight2.classList.toggle("red");
-//         redLight2.classList.toggle("green");
-//         greenLight2.classList.toggle("red");    
-//         greenLight2.classList.toggle("green");
+// //     redLightSecond.classList.add("green_second");
+// //     redLightSecond.classList.remove("red_second");
+// //     greenLightSecond.classList.add("red_second");
+// //     greenLightSecond.classList.remove("green_second");
 
-//         greenRoad.classList.toggle("red_road");
-//         greenRoad.classList.toggle("green_road");
-//         redRoad.classList.toggle("green_road");
-//         redRoad.classList.toggle("red_road");
+// //     let greenRoad = document.querySelector(".green_road");
+// //     let redRoad = document.querySelector(".red_road");
+
+// //     greenRoad.classList.add("red_road");
+// //     greenRoad.classList.remove("green_road");
+// //     redRoad.classList.add("green_road");
+// //     redRoad.classList.remove("red_road");
+// // })
+
+
+// // let alarmButton = document.querySelector(".alarm_button");
+// // alarmButton.addEventListener("click", function () {
+// //     let redLight = document.querySelector("#redId");
+// //     let greenLight = document.querySelector("#greenId");
+// //     let redLight2 = document.querySelector("#redId2");
+// //     let greenLight2 = document.querySelector("#greenId2");
+// //     let greenRoad = document.querySelector(".green_road");
+// //     let redRoad = document.querySelector(".red_road");
+// //     if (redLight.classList.contains("red")||redLight.classList.contains("green")&&greenLight.classList.contains("green")||greenLight.classList.contains("red")) {
+// //         redLight.classList.toggle("green");
+// //         redLight.classList.toggle("red");
+// //         greenLight.classList.toggle("red");
+// //         greenLight.classList.toggle("green");
+
+// //         redLight2.classList.toggle("red");
+// //         redLight2.classList.toggle("green");
+// //         greenLight2.classList.toggle("red");    
+// //         greenLight2.classList.toggle("green");
+
+// //         greenRoad.classList.toggle("red_road");
+// //         greenRoad.classList.toggle("green_road");
+// //         redRoad.classList.toggle("green_road");
+// //         redRoad.classList.toggle("red_road");
         
-//     }
+// //     }
 
+// // })
+
+// let main = document.getElementById('main');
+// main.addEventListener("click", function() {
+//     // console.log(event.target.tagName);
 // })
+// let red = document.getElementById('red');
+// red.addEventListener("click", function() {
+//     console.log("red");
+// })
+// let p = document.querySelector('p');
+// p.addEventListener("click", function() {
+//     console.log("P");
+// });
+// let h = document.querySelector('h1');
+// h.addEventListener("click", function() {
+//     console.log("H1");
+// });
+// let u = document.querySelector('ul');
+// u.addEventListener("click", function() {
+//     console.log("UL");
+// });
 
-let main = document.getElementById('main');
-main.addEventListener("click", function() {
-    // console.log(event.target.tagName);
-})
-let red = document.getElementById('red');
-red.addEventListener("click", function() {
-    console.log("red");
-})
-let p = document.querySelector('p');
-p.addEventListener("click", function() {
-    console.log("P");
-});
-let h = document.querySelector('h1');
-h.addEventListener("click", function() {
-    console.log("H1");
-});
-let u = document.querySelector('ul');
-u.addEventListener("click", function() {
-    console.log("UL");
-});
-
-let l = document.querySelector("ul>li");
-l.addEventListener("click",function() {
-    console.log("LI");
-});
+// let l = document.querySelector("ul>li");
+// l.addEventListener("click",function() {
+//     console.log("LI");
+// });
 
 
+
+var Person = function(name) {
+    this.name = name;
+    this.canTalk = true;
+    this.greet = function() {
+        if (this.canTalk) {
+            console.log('Привет, я ' + this.name);
+        }
+    };
+};
+
+var Employee = function(name, title) {
+    this.name = name;
+    this.title = title;
+    this.greet = function() {
+        if (this.canTalk) {
+            console.log('Привет, я ' + this.name + ', ' + this.title);
+        }
+    };
+};
+Employee.prototype = new Person();
+
+var Customer = function(name) {
+    this.name = name;
+};
+Customer.prototype = new Person();
+
+var Mime = function(name) {
+    this.name = name;
+    this.canTalk = false;
+};
+Mime.prototype = new Person();
+
+var bob = new Employee('Боб', 'Строитель');
+var joe = new Customer('Джо');
+var rg = new Employee('Ред Грин', 'Разнорабочий');
+var mike = new Customer('Майк');
+var mime = new Mime('Мим');
+bob.greet();
+joe.greet();
+rg.greet();
+mike.greet();
+mime.greet();
